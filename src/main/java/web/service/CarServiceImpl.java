b65b97accdac838cc118e7887d7ef55a66636026
itmagnat-ru <117728092+itmagnat-ru@users.noreply.github.com>
2023-02-15 12:25:57 +0400

    tst

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -9,8 +9,11 @@ import java.util.List;
 
 @Service
 public class CarServiceImpl implements CarService {
-    @Autowired
-    private CarDao carDao;
+    private final CarDao carDao;
+
+    public CarServiceImpl(CarDao carDao) {
+        this.carDao = carDao;
+    }
 
     @Override
     public List<Car> getCars(int count) {
